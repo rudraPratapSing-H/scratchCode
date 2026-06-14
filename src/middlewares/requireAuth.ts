77b950a6f0c62c07cd7ef0c672475300bf01a051
Rudra Pratap Singh <24bcs156@ietdavv.edu.in>
2026-06-14 15:14:40 +0530

    turned coockies sameSite=None

--- src/middlewares/requireAuth.ts
+++ src/middlewares/requireAuth.ts
@@ -49,7 +49,7 @@ export async function requireAuth(req: Request, res: Response, next: NextFunctio
         res.cookie('refreshToken', refreshed.refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
